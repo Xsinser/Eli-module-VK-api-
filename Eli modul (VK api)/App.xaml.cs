d1db072e4a7851506ce65fd3131354411d1ffada
Xsinser <xsinser175@gmail.com>
2019-09-30 14:31:28 +0300

    Добавьте файлы проекта.

---
+++ Eli modul (VK api)/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Eli_modul__VK_api_
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
